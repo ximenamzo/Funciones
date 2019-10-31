8d3a161edb344e0250c9cbe793015043f4d488d6
Ximena Manzo <ximenamzo1994@gmail.com>
2019-10-31 11:45:12 -0600

    1.2

--- Funciones/Funciones.cpp
+++ Funciones/Funciones.cpp
@@ -12,6 +12,53 @@ void pausa();
 
 int main()
 {
-
+	menu();
 	return 0;
+}
+//Creamos funciones
+void menu()
+{
+	bool bandera = false;
+	char tecla;
+	do
+	{
+		system("cls");
+		cin.clear();
+		cout << "Calculadora con funciones" << endl;
+		cout << "-------------------------" << endl;
+		cout << "\t1.- Sumar" << endl;
+		cout << "\t2.- Restar" << endl;
+		cout << "\t3.- Multiplicar" << endl;
+		cout << "\t4.- Dividir" << endl;
+		cout << "\t5.- Salir" << endl;
+		cout << "Elije una opcion: " << endl;
+		cin >> tecla;
+		switch (tecla)
+		{
+		case '1':
+			system("cls");
+			suma();
+			break;
+		case '2':
+			system("cls");
+			resta();
+			break;
+		case '3':
+			system("cls");
+			multi();
+			break;
+		case '4':
+			system("cls");
+			division();
+			break;
+		case '5':
+			bandera = true;
+			break;
+		default:
+			system("cls");
+			cout << "Opcion no valida" << endl;
+			pausa();
+			break;
+		}
+	} while (bandera != true);
 }
